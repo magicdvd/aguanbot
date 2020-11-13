7125c33f8309d23fbe6b2ff422c5c0c3d046e7ec
huyouzhang <mason@dhms.com.cn>
2020-11-13 14:08:25 +0800

    update

--- service/api_test.go
+++ service/api_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestBot_Login(t *testing.T) {
 	a := NewAguanAPI(log.NewStdLogger("debug"))
-	b, err := a.Login(context.TODO(), "18600000000", "sd7155917")
+	b, err := a.Login(context.TODO(), "", "")
 	if err != nil {
 		t.Error(err)
 	}
